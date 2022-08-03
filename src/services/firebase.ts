c73c986e6cce15ba8fb6445c35654c287c8fdf30
Joao <joaohbalbernaz@gmail.com>
2022-08-03 10:13:22 -0300

    fix: firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
